b7791214402c34811750441c69ebd770d903acce
Saranhan <saranhanthurairasa@gmail.com>
2023-11-24 12:24:34 +0100

    feat: started writing header

---
+++ password-manager.ps1
@@ -0,0 +1,21 @@
+<# 
+   Modul 129 LB2 - Password Manager Script
+
+
+   Description:
+   PowerShell Password Manager is a simple yet secure password management tool developed in PowerShell. 
+   This project provides a command-line interface (CLI) for users to add and retrieve passwords securely stored in an SQLite database. 
+   The tool includes features for initializing the database, adding new passwords, and retrieving passwords for specific services.
+    
+
+   Author:
+   hpa134085@stud.gibb.ch
+   sth134864@stud.gibb.ch
+
+   Date:
+   22. December 2023
+
+   Version
+   1.0.1
+
+   To-Do:
